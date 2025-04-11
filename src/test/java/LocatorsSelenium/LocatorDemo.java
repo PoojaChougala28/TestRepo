4c9693333e3aaecb64a477332bf720ee09114a47
PoojaChougala28 <poojaachougala28@gmail.com>
2025-04-11 16:00:03 +0530

    selenium push

--- src/test/java/LocatorsSelenium/LocatorDemo.java
+++ src/test/java/LocatorsSelenium/LocatorDemo.java
@@ -5,8 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.util.List;
-
 public class LocatorDemo
 {
     public static void main(String[] args) throws InterruptedException {
@@ -20,7 +18,7 @@ public class LocatorDemo
         driver.findElement(By.id("datepicker")).sendKeys("08/04/2025");
         driver.findElement(By.id("profession-0")).click();
         driver.findElement(By.id("profession-1")).click();
-        WebElement checkbox=driver.findElement(By.className("control-group"));
+        //WebElement checkbox=driver.findElement(By.className("control-group"));
         driver.findElement(By.id("tool-2")).click();
         driver.findElement(By.id("tool-1")).click();
         driver.findElement(By.className("input-xlarge")).sendKeys("Africa");
